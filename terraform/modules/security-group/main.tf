4cf41d41f87bb06341777e6e42015111ac7c821e
Marwan Hesham <104536014+Marwan933@users.noreply.github.com>
2024-12-28 23:25:59 +0200

    Update main.tf

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -15,7 +15,14 @@ resource "aws_security_group_rule" "rules" {
   cidr_blocks = each.value.cidr_blocks
   security_group_id = aws_security_group.sg.id
 }
-
+resource "aws_security_group_rule" "egress_rule" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg.id
+}
 output "security_group_id" {
   value = aws_security_group.sg.id
 }
